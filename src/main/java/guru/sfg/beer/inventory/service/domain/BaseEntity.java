019853fcd252278fc4ad1504d0935f0d8fe027d2
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-24 14:09:45 -0500

    128. Enhance Get Beer with Inventory Information.

--- src/main/java/guru/sfg/beer/inventory/service/domain/BaseEntity.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BaseEntity.java
@@ -47,10 +47,7 @@ public class BaseEntity {
 
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Type(type="org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false )
     private UUID id;
